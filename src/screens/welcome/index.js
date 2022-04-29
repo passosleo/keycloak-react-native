da0337c231971bfdba02f2cd9b0f85f28864801e
passosleo <leonardopassos98@gmail.com>
2022-04-29 10:59:42 -0300

    feat: auth conditional rendering

--- src/screens/welcome/index.js
+++ src/screens/welcome/index.js
@@ -1,41 +1,57 @@
-import React from "react";
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
-import { useKeycloak } from "@react-keycloak/native";
-import logo from "../../assets/images/react-keycloak-logo.png";
+import React from 'react';
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import {useKeycloak} from '@react-keycloak/native';
+import logo from '../../assets/images/react-keycloak-logo.png';
 
 const Welcome = () => {
   const [keycloak, initialized] = useKeycloak();
   return (
     <View style={styles.container}>
-      <Image source={logo} />
-      <Text style={styles.title}>Keycloak React Native Demo App</Text>
-      <TouchableOpacity onPress={() => keycloak.login()} style={styles.button}>
-        <Text style={styles.text}>Entrar</Text>
-      </TouchableOpacity>
+      {!keycloak.authenticated ? (
+        <>
+          <Image source={logo} />
+          <Text style={styles.title}>Keycloak React Native Demo App</Text>
+          <TouchableOpacity
+            onPress={() => keycloak.login()}
+            style={styles.button}>
+            <Text style={styles.text}>Login</Text>
+          </TouchableOpacity>
+        </>
+      ) : (
+        <>
+          <Text>{`Welcome ${keycloak.tokenParsed.name} - ${keycloak.tokenParsed.email}`}</Text>
+          <Text>{`Your JWT token is: ${keycloak.token}`}</Text>
+          <TouchableOpacity
+            onPress={() => keycloak.logout()}
+            style={styles.button}>
+            <Text style={styles.text}>Logout</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#d3dedc",
+    backgroundColor: '#d3dedc',
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 22,
-    color: "#000",
+    color: '#000',
     marginVertical: 64,
   },
   button: {
-    backgroundColor: "#00a5e5",
+    backgroundColor: '#00a5e5',
     paddingHorizontal: 64,
     paddingVertical: 12,
     borderRadius: 12,
   },
   text: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 18,
   },
 });
